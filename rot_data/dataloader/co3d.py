41c710247f4f40ea306ee91b5d25afd0c846e16c
pufanyi <pufanyi@sensetime.com>
2025-10-01 02:27:06 +0000

    Refactor CO3DDataLoader initialization for improved readability and update co3d.json to ensure proper formatting.

--- rot_data/dataloader/co3d.py
+++ rot_data/dataloader/co3d.py
@@ -1,12 +1,18 @@
-from .data import Data, DataLoader
-from pathlib import Path
 import json
 import os
-from typing import Iterable
+from collections.abc import Iterable
+from pathlib import Path
+
+from .data import Data, DataLoader
 
 
 class CO3DDataLoader(DataLoader):
-    def __init__(self, num_frames: int = 6, cache_dir: str | os.PathLike = "cache", num_threads: int = 4):
+    def __init__(
+        self,
+        num_frames: int = 6,
+        cache_dir: str | os.PathLike = "cache",
+        num_threads: int = 4,
+    ):
         self.links_file = Path(__file__).parents[1] / "links" / "co3d.json"
         with self.links_file.open() as f:
             self.links = json.load(f)
@@ -14,5 +20,6 @@ class CO3DDataLoader(DataLoader):
         self.cache_dir = Path(cache_dir).absolute()
         self.cache_dir.mkdir(parents=True, exist_ok=True)
         self.num_threads = num_threads
+
     def load(self) -> Iterable[Data]:
         pass
