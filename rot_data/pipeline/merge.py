bba4a40d69e0f0d14702aed1e7c7663deb52c15e
pufanyi <pufanyi@sensetime.com>
2025-10-03 09:44:20 +0000

    Refactor prepare_data.py to save dataset in jsonl format and manage image storage

---
+++ rot_data/pipeline/merge.py
@@ -0,0 +1,20 @@
+import json
+import datasets
+from PIL import Image
+from pathlib import Path
+
+def dataset_generator():
+    folder = Path("output")
+    count = 0
+    with open("output/data.jsonl", "r") as f:
+        for line in f:
+            data = json.loads(line)
+            data["images"] = [Image.open(folder / image) for image in data["images"]]
+            data["predict_image"] = Image.open(folder / data["predict_image"])
+            yield data
+            count += 1
+
+
+if __name__ == "__main__":
+    dataset = datasets.Dataset.from_generator(dataset_generator, keep_in_memory=True)
+    dataset.push_to_hub("pufanyi/co3d")
